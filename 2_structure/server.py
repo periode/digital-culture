ad0ebbe1d84b525d5eb51753a3358a34414e113f
pierre <pierre.depaz@gmail.com>
2019-09-27 23:18:20 +0200

    added template html and serving html from python server

--- 2_structure/server.py
+++ 2_structure/server.py
@@ -6,4 +6,8 @@ server = Flask(__name__)
 def respond():
     return "what's up"
 
+@server.route('/page')
+def respondWithHTML():
+    return "<h1>THIS IS HTML</h1>"
+
 server.run(host="0.0.0.0", port=80)
