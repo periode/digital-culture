14b45beff8afe0f4b07466577b548907c588c2e6
période <pierre.depaz@gmail.com>
2025-08-25 19:23:41 +0200

    cleaned up docs

--- docs/memes-language/heatmap/static/js/xlsx.a0ee68df.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpheat-map"]=this["webpackJsonpheat-map"]||[]).push([[2],{579:function(n,p){},590:function(n,p){},591:function(n,p){}}]);
-//# sourceMappingURL=xlsx.a0ee68df.chunk.js.map
